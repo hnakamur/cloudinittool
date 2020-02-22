9c6f2351428d0d972f1cf1e1ffef52b8e9000599
Hiroaki Nakamura <hnakamur@gmail.com>
2020-02-22 17:06:29 +0900

    Add example config and scripts for multipass

---
+++ multipass/launch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ssh-keygen -t ed25519 -f ~/.ssh/vm.id_ed25519 -C vm
+cloudinittool modify-user-data -in user-data.in.yml -out user-data -passwd -pub-key ~/.ssh/vm.id_ed25519.pub
+multipass launch --name primary --cpus 2 --mem 4G --disk 100G --cloud-init user-data
