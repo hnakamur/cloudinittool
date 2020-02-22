e6591860c0f5a93d11158e0ce7b01e9434e56efd
Hiroaki Nakamura <hnakamur@gmail.com>
2020-02-22 17:15:17 +0900

    Add multipass start after multipass launch

--- multipass/launch.sh
+++ multipass/launch.sh
@@ -2,3 +2,4 @@
 ssh-keygen -t ed25519 -f ~/.ssh/vm.id_ed25519 -C vm
 cloudinittool modify-user-data -in user-data.in.yml -out user-data -passwd -pub-key ~/.ssh/vm.id_ed25519.pub
 multipass launch --name primary --cpus 2 --mem 4G --disk 100G --cloud-init user-data
+multipass start primary
